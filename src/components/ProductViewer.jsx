6d56222deae7a55686ab6660b47fe96584c516f7
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-04 18:08:54 +0530

    MODELS REndering

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -61,7 +61,7 @@ const ProductViewer = () => {
 
       <Canvas id='canvas'  camera ={{position :[0,2,5] , fov:50 , near :0.1, far:100}}>
             <StudioLights/>
-            <MacbookModel14 scale ={0.06} position={[0 , 0 , 0]}/>
+           
         
         <ModelSwitcher  scale={isMobile? scale -0.03 :scale} isMobile = {isMobile} />
       </Canvas>
