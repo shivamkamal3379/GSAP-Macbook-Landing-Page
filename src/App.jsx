9099c2e9aa67a14f8ddab63390f029a0c259518a
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-04 14:02:38 +0530

    implemented product viewer with clsx and zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 const App = () => {
   return (
    <main>
     <NavBar/>
     <Hero/>
+    <ProductViewer/>
    </main>
   )
 }
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import {create} from 'zustand' ;
+
+const useMacbookStore = create((set)=>({
+    color: '#2e2c2e',
+    setColor : (color) => set({color}),
+    scale: 0.08 , 
+    setScale : (scale)=> set({scale}),
+
+    reset:()=>  set({color : "#2c2e2c" , scale :0.08})
+}))
+
+export default useMacbookStore;
