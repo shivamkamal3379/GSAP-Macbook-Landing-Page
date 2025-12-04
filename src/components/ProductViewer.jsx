849a45c907bad0a0223b8bd521b477ee2be1c937
Shivam Kamal <shivamkamal3379@gmail.com>
2025-12-04 15:07:11 +0530

    OrbitControls , Box and Canvas Implemented

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import useMacbookStore   from "../store"
 import clsx from 'clsx';
+import {Canvas} from "@react-three/fiber"
+import { Box } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
+
+
 
 
 const ProductViewer = () => {
@@ -47,8 +52,17 @@ const ProductViewer = () => {
         </div>
       </div>
       
+{/* 
+      <Canvas id='canvas'>
+            <box position = {[-1,0,0]}>
 
-      <p className='text-white text-4xl'> Render Canavas</p>
+            </box>
+      </Canvas> */}
+      <Canvas id='canvas'  camera ={{position :[0,2,5] , fov:50 , near :0.1, far:100}}>
+
+        <Box position ={[0 , 0 , 0]} scale ={10* scale} material-color={color}/>
+        <OrbitControls enableZoom = {false}/>
+      </Canvas>
     </section>
   )
 }
